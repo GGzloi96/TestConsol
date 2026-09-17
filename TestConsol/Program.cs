d2c72fb6b1502aab6a48711ee0e988ca2e4f7f75
GGzloi96 <kenackiken@yandex.ru>
2026-09-17 21:32:49 +0700

    Жесткий метод по выводу суммы

--- TestConsol/Program.cs
+++ TestConsol/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+namespace TestConsol
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 2;
+
+            Console.WriteLine(a + b);
+
+        }
+    }
+   
+
+    
+}
