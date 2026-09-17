f51b7b73fc6798d11b0518eb999c42571af2eeb6
Kusok Ukropa <puvelgubenko82844@gmail.com>
2026-09-17 21:39:33 +0700

    Многа функция

--- TestConsol/Program.cs
+++ TestConsol/Program.cs
@@ -1,23 +1,25 @@
 ﻿using System;
+using System.Numerics;
+
 namespace TestConsol
-{
-    public class Program
+{ 
+    public class Programm
     {
         public static void Main(string[] args)
         {
             int a = 1;
             int b = 2;
 
-            Console.WriteLine(Minus(a,b));
-
+            Console.WriteLine(Plus(a, b));
         }
 
+        private static int Plus(int a, int b)
+        {
+            return a + b;
+        }
         private static int Minus(int x, int y)
         {
             return x - y;
         }
     }
-   
-
-    
 }
